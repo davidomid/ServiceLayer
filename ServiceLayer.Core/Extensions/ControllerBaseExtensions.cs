9bff2f129734d814362489320102ab9f48647273
David Omid <davidomid@hotmail.com>
2018-05-07 22:28:59 +0100

    Added ControllerBaseExtensions extensions class, new ServiceLayer.Core project

---
+++ ServiceLayer.Core/Extensions/ControllerBaseExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceLayer.Core.Extensions
+{
+    public static class ControllerBaseExtensions
+    {
+        public static ActionResult FromResult<T>(this ControllerBase controller, ServiceResult<T> result)
+        {
+            switch (result.ResultType)
+            {
+                case ServiceResultTypes.Ok:
+                    return controller.Ok(result.Data);
+                case ServiceResultTypes.NotFound:
+                    return controller.NotFound();
+                case ServiceResultTypes.BadRequest:
+                    return controller.BadRequest(result.ErrorMessages);
+                case ServiceResultTypes.Error:
+                    return controller.StatusCode(500, result.ErrorMessages);
+                default:
+                    throw new ArgumentOutOfRangeException(
+                        $"No action result could be returned for service result type {result.ResultType}");
+            }
+        }
+    }
+}
