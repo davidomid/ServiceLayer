582b595ac115b506b45a5b514c1a1555a600b27f
David Omid <davidomid@hotmail.com>
2019-05-30 00:31:35 +0100

    Added new IResultTypeConversionService interface with comments

---
+++ ServiceLayer/Internal/Converters/IResultTypeConversionService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceLayer.Internal.Converters
+{
+    internal interface IResultTypeConversionService
+    {
+        // The following algorithm should be used for conversion:
+        // Check if there is a particular rule setup for converting from TSourceResultType to TDestinationSourceResultType
+        // If there is, use it.
+        // If there isn't, check if there is a general rule for converting from TSourceResultType to any Enum in general.
+        // If there is, use it.
+        // If there isn't, check if there is a rule for converting to TDestinationSourceResultType from any Enum in general.
+        // If there is, use it.
+        // Otherwise, throw exception.
+        // If the rule itself fails, we throw another exception.
+        TDestinationResultType Convert<TSourceResultType, TDestinationResultType>(TSourceResultType sourceResultType) where TDestinationResultType : struct, Enum;
+    }
+}
