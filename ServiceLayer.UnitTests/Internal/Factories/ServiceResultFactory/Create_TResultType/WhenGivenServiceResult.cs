4f132dd85e81b4b630d823282f9febaff046abcc
David Omid <davidomid@hotmail.com>
2019-05-29 18:39:08 +0100

    Updated test

--- ServiceLayer.UnitTests/Internal/Factories/ServiceResultFactory/Create_TResultType/WhenGivenServiceResult.cs
+++ ServiceLayer.UnitTests/Internal/Factories/ServiceResultFactory/Create_TResultType/WhenGivenServiceResult.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 using FluentAssertions;
+using Moq;
 using NUnit.Framework;
+using ServiceLayer.Converters;
 using Testing.Common.Domain.TestClasses;
 
 namespace ServiceLayer.UnitTests.Internal.Factories.ServiceResultFactory.Create_TResultType
@@ -18,6 +20,8 @@ namespace ServiceLayer.UnitTests.Internal.Factories.ServiceResultFactory.Create_
 
         private readonly ServiceResultTypes _resultType;
 
+        private readonly TestCustomServiceResultTypes _expectedCustomResultType = (TestCustomServiceResultTypes) 1000;
+
         private object[] _errorDetails; 
 
         private static readonly ServiceResultTypes[] ResultTypes = (ServiceResultTypes[])Enum.GetValues(typeof(ServiceResultTypes));
@@ -29,6 +33,9 @@ namespace ServiceLayer.UnitTests.Internal.Factories.ServiceResultFactory.Create_
 
         protected override void Arrange()
         {
+            Mock<IResultTypeConverter<ServiceResultTypes, TestCustomServiceResultTypes>> mockConverter = new Mock<IResultTypeConverter<ServiceResultTypes, TestCustomServiceResultTypes>>();
+            mockConverter.Setup(c => c.Convert(_resultType)).Returns(_expectedCustomResultType); 
+            ServiceResultConfiguration.ResultTypeConverters.Specific.AddOrReplace(mockConverter.Object);
             _errorDetails = new[] {"test 123", 123, new object()};
             _fromServiceResult = new ServiceResult(_resultType, _errorDetails);
         }
@@ -53,7 +60,7 @@ namespace ServiceLayer.UnitTests.Internal.Factories.ServiceResultFactory.Create_
         [Test]
         public void Should_Return_Result_With_Expected_ResultType()
         {
-            _serviceResult.ResultType.Should().Be(_resultType.ToResultType<TestCustomServiceResultTypes>());
+            _serviceResult.ResultType.Should().Be(_expectedCustomResultType);
         }
     }
 }
