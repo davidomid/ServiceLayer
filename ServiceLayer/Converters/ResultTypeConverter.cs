945aaabcac2fbbfebc9a446ae270358a718be5bd
David Omid <davidomid@hotmail.com>
2019-06-07 20:24:30 +0100

    Refactor

--- ServiceLayer/Converters/ResultTypeConverter.cs
+++ ServiceLayer/Converters/ResultTypeConverter.cs
@@ -48,9 +48,5 @@ namespace ServiceLayer.Converters
                     ex);
             }
         }
-
-        Type IResultTypeConverter.SourceType => SourceType;
-
-        Type IResultTypeConverter.DestinationType => DestinationType;
     }
 }
