6d20b4649a029bf6620607de19d81eb2d63be1ca
David Omid <davidomid@hotmail.com>
2020-02-29 23:11:38 +0000

    Implemented Uninstall method for AspNetCorePlugin

--- ServiceLayer.Core/AspNetCorePlugin.cs
+++ ServiceLayer.Core/AspNetCorePlugin.cs
@@ -4,14 +4,30 @@ namespace ServiceLayer.Core
 {
     public class AspNetCorePlugin : Plugin
     {
+        private readonly ResultTypeConverter[] _resultTypeConverters = 
+        {
+            new HttpStatusCodeToResultTypesConverter(),
+            new ResultTypesToHttpStatusCodeConverter()
+        };
+
         public AspNetCorePlugin() : base("ASP.NET Core")
         {
         }
 
         public override void Install()
         {
-            Context.ResultTypeConverters.Install(new HttpStatusCodeToResultTypesConverter());
-            Context.ResultTypeConverters.Install(new ResultTypesToHttpStatusCodeConverter());
+            foreach (var resultTypeConverter in _resultTypeConverters)
+            {
+                Context.ResultTypeConverters.Install(resultTypeConverter);
+            }
+        }
+
+        public override void Uninstall()
+        {
+            foreach (var resultTypeConverter in _resultTypeConverters)
+            {
+                Context.ResultTypeConverters.Uninstall(resultTypeConverter);
+            }
         }
     }
 }
