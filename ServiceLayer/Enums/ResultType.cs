6db8fe84b7cf98bad9a01d64b13822b5ca919074
David Omid <davidomid@hotmail.com>
2021-10-13 18:51:06 +0100

    Added comment documentation to ResultType enum

--- ServiceLayer/Enums/ResultType.cs
+++ ServiceLayer/Enums/ResultType.cs
@@ -1,10 +1,22 @@
-﻿namespace ServiceLayer
+namespace ServiceLayer
 {
+    /// <summary>
+    /// The default enum representing the status of a result returned from a service method.
+    /// </summary>
     public enum ResultType
     {
+        /// <summary>
+        /// Indicates that the result is neither a success or a failure. <b>This is the default value.</b>
+        /// </summary>
         [DefaultResultType]
-        Inconclusive,
-        Success,
-        Failure
+        Inconclusive = 0,
+        /// <summary>
+        /// Indicates that the result is a success.
+        /// </summary>
+        Success = 1,
+        /// <summary>
+        /// Indicates that the result is a failure.
+        /// </summary>
+        Failure = 2
     }
 }
